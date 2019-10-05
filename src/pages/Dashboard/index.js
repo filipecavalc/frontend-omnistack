d6ba179d1e6aa0078c0c909e5e1eae68c3c60524
filipecavalc <filipecavalc@gmail.com>
2019-10-05 20:05:01 -0300

    login basic function and dashboard navigation

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import api from '../../Services/api'
+export default function Dashboard() {
+    useEffect(() => {
+        
+
+    }, [])
+    return <h1>Dashboard</h1>
+}
