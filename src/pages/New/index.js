9d790035134393bee97805c8499c328fce9910c8
filipecavalc <filipecavalc@gmail.com>
2019-10-05 20:58:01 -0300

    company list from user

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function New() {
-    return <div />
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+
+    function handleSubmit() {
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="company">EMPRESA *</label>
+            <input 
+                id="company"
+                placeholder="Sua empresa incrível"
+                value={company}
+                onChange={event => setCompany(event.target.value)}
+            />
+
+            <label htmlFor="techs">TECNOLOGIAS * <span>(separadas por vírgula)</span></label>
+            <input 
+                id="techs"
+                placeholder="Sua empresa incrível"
+                value={techs}
+                onChange={event => setTechs(event.target.value)}
+            />
+
+            <label htmlFor="techs">VALOR DA DIÁRIA * <span>(Caso gratuito deixe em branco)</span></label>
+            <input 
+                id="techs"
+                placeholder="Sua empresa incrível"
+                value={techs}
+                onChange={event => setTechs(event.target.value)}
+            />
+        </form>
+    )
 }
